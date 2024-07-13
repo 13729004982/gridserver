7b6d7ee601a741c7b75c500a1f77cc7493e4ded6
wq <1295012067@qq.com>
2024-07-13 16:28:00 +0800

    修改为基于http协议的服务器

--- server.cpp
+++ server.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <microhttpd.h>
 #include <json/json.h>
 #include "server.h"
 
@@ -17,106 +14,104 @@ Server::Server(const std::string& host, const std::string& user, const std::stri
 
 Server::~Server() 
 {
-    delete db;
+    delete m_db;
+}
+
+static int send_response(struct MHD_Connection *connection, const std::string& response, int status_code) 
+{
+    struct MHD_Response *mhd_response;
+    mhd_response = MHD_create_response_from_buffer(response.size(), (void*)response.c_str(), MHD_RESPMEM_MUST_COPY);
+    int ret = MHD_queue_response(connection, status_code, mhd_response);
+    MHD_destroy_response(mhd_response);
+    return ret;
+}
+
+static int handle_request(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) 
+{
+    Server* server = static_cast<Server*>(cls);
+    if (strcmp(method, "POST") != 0) 
+    {
+        return MHD_NO;
+    }
+
+    static int dummy;
+    if (&dummy != *con_cls) 
+    {
+        *con_cls = &dummy;
+        return MHD_YES;
+    }
+
+    if (*upload_data_size != 0) 
+    {
+        std::string request_data(upload_data, *upload_data_size);
+        *upload_data_size = 0;
+
+        Json::Reader reader;
+        Json::Value request;
+        reader.parse(request_data, request);
+
+        std::string action = request["action"].asString();
+        std::string response;
+
+        if (action == "get") 
+        {
+            std::string id = request["id"].asString();
+            sql::ResultSet* res = server->m_db->query("SELECT * FROM machines WHERE id = '" + id + "'");
+            Json::Value result;
+            if (res->next()) 
+            {
+                result["id"] = res->getString("id");
+                result["ip"] = res->getString("ip");
+                result["port"] = res->getInt("port");
+                result["voltage"] = res->getDouble("voltage");
+                result["daily_usage"] = res->getDouble("daily_usage");
+                result["monthly_usage"] = res->getDouble("monthly_usage");
+                result["last_update"] = res->getString("last_update");
+                result["last_query"] = res->getString("last_query");
+            }
+            delete res;
+            Json::StreamWriterBuilder writer;
+            response = Json::writeString(writer, result);
+        } 
+        else if (action == "update") 
+        {
+            std::string id = request["id"].asString();
+            std::string ip = request["ip"].asString();
+            int port = request["port"].asInt();
+            double voltage = request["voltage"].asDouble();
+            double daily_usage = request["daily_usage"].asDouble();
+            double monthly_usage = request["monthly_usage"].asDouble();
+            std::string last_update = request["last_update"].asString();
+            std::string last_query = request["last_query"].asString();
+
+            std::string query = "UPDATE machines SET ip='" + ip + "', port=" + std::to_string(port) + 
+                                ", voltage=" + std::to_string(voltage) + ", daily_usage=" + std::to_string(daily_usage) + 
+                                ", monthly_usage=" + std::to_string(monthly_usage) + ", last_update='" + last_update + 
+                                "', last_query='" + last_query + "' WHERE id='" + id + "'";
+            server->m_db->update(query);
+            response = "{\"status\":\"success\"}";
+        }
+
+        return send_response(connection, response, MHD_HTTP_OK);
+    }
+
+    return MHD_NO;
 }
 
 void Server::initAndRun() 
 {
-    int server_fd;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    struct MHD_Daemon *daemon;
+    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &handle_request, this, MHD_OPTION_END);
+    if (NULL == daemon) 
     {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+        std::cerr << "Failed to start HTTP server" << std::endl;
+        return;
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    std::cout << "HTTP server running on port " << PORT << std::endl;
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    // 保持服务器运作
+    getchar();
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0) 
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    while (true) {
-        if ((m_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        handleClient(m_socket, m_db);
-    }
-}
-
-void Server::handleClient(MySQLDatabase* db) 
-{
-    char buffer[1024] = {0};
-    read(m_socket, buffer, 1024);
-
-    Json::Reader reader;
-    Json::Value request;
-    reader.parse(buffer, request);
-
-    std::string action = request["action"].asString();
-    std::string response;
-
-    if (action == "get") 
-    {
-        std::string id = request["id"].asString();
-        sql::ResultSet* res = db.query("SELECT * FROM machines WHERE id = '" + id + "'");
-        Json::Value result;
-        if (res->next()) 
-        {
-            result["id"] = res->getString("id");
-            result["ip"] = res->getString("ip");
-            result["port"] = res->getInt("port");
-            result["voltage"] = res->getDouble("voltage");
-            result["daily_usage"] = res->getDouble("daily_usage");
-            result["monthly_usage"] = res->getDouble("monthly_usage");
-            result["last_update"] = res->getString("last_update");
-            result["last_query"] = res->getString("last_query");
-        }
-        delete res;
-        Json::StreamWriterBuilder writer;
-        response = Json::writeString(writer, result);
-    } 
-    else if (action == "update") 
-    {
-        std::string id = request["id"].asString();
-        std::string ip = request["ip"].asString();
-        int port = request["port"].asInt();
-        double voltage = request["voltage"].asDouble();
-        double daily_usage = request["daily_usage"].asDouble();
-        double monthly_usage = request["monthly_usage"].asDouble();
-        std::string last_update = request["last_update"].asString();
-        std::string last_query = request["last_query"].asString();
-
-        std::string query = "UPDATE machines SET ip='" + ip + "', port=" + std::to_string(port) + 
-                            ", voltage=" + std::to_string(voltage) + ", daily_usage=" + std::to_string(daily_usage) + 
-                            ", monthly_usage=" + std::to_string(monthly_usage) + ", last_update='" + last_update + 
-                            "', last_query='" + last_query + "' WHERE id='" + id + "'";
-        db.update(query);
-        response = "{\"status\":\"success\"}";
-    }
-
-    send(m_socket, response.c_str(), response.size(), 0);
-    close(m_socket);
+    MHD_stop_daemon(daemon);
 }
